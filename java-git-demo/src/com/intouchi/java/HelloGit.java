21b9720276c7706c5c3841078c315d8570b8e6c7
guoyu <715371669@qq.com>
2020-12-03 09:51:21 +0800

    实施第二次修改

---
+++ java-git-demo/src/com/intouchi/java/HelloGit.java
@@ -0,0 +1,9 @@
+package com.intouchi.java;
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello,Git!");
+        System.out.println("update1");
+        System.out.println("update2");
+    }
+}
